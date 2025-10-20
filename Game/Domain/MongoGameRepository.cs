97e66fed88b893e8c666ed65fc85df4b94783328
mohovikov <mokhovikov@tochka.com>
2025-10-20 16:21:46 +0500

    add task 2 and 3

--- Game/Domain/MongoGameRepository.cs
+++ Game/Domain/MongoGameRepository.cs
@@ -4,42 +4,55 @@ using MongoDB.Driver;
 
 namespace Game.Domain
 {
-    // TODO Сделать по аналогии с MongoUserRepository
     public class MongoGameRepository : IGameRepository
     {
         public const string CollectionName = "games";
+        private readonly IMongoCollection<GameEntity> gameCollection;
 
         public MongoGameRepository(IMongoDatabase db)
         {
+            gameCollection = db.GetCollection<GameEntity>(CollectionName);
         }
 
         public GameEntity Insert(GameEntity game)
         {
-            throw new NotImplementedException();
+            gameCollection.InsertOne(game);
+            return game;
         }
 
         public GameEntity FindById(Guid gameId)
         {
-            throw new NotImplementedException();
+            var filter = Builders<GameEntity>.Filter.Eq(g => g.Id, gameId);
+            return gameCollection.Find(filter).FirstOrDefault();
         }
 
         public void Update(GameEntity game)
         {
-            throw new NotImplementedException();
+            var filter = Builders<GameEntity>.Filter.Eq(g => g.Id, game.Id);
+            var options = new ReplaceOptions { IsUpsert = false };
+            gameCollection.ReplaceOne(filter, game, options);
         }
 
         // Возвращает не более чем limit игр со статусом GameStatus.WaitingToStart
         public IList<GameEntity> FindWaitingToStart(int limit)
         {
-            //TODO: Используй Find и Limit
-            throw new NotImplementedException();
+            var filter = Builders<GameEntity>.Filter.Eq(g => g.Status, GameStatus.WaitingToStart);
+            return gameCollection
+                .Find(filter)
+                .Limit(limit)
+                .ToList();
         }
 
         // Обновляет игру, если она находится в статусе GameStatus.WaitingToStart
         public bool TryUpdateWaitingToStart(GameEntity game)
         {
-            //TODO: Для проверки успешности используй IsAcknowledged и ModifiedCount из результата
-            throw new NotImplementedException();
+            var filter = Builders<GameEntity>.Filter.And(
+                Builders<GameEntity>.Filter.Eq(g => g.Id, game.Id),
+                Builders<GameEntity>.Filter.Eq(g => g.Status, GameStatus.WaitingToStart)
+            );
+            
+            var result = gameCollection.ReplaceOne(filter, game);
+            return result.IsAcknowledged && result.ModifiedCount > 0;
         }
     }
 }
--- Game/Domain/MongoUserRepositoty.cs
+++ Game/Domain/MongoUserRepositoty.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using MongoDB.Driver;
 
 namespace Game.Domain
@@ -11,43 +12,73 @@ namespace Game.Domain
         public MongoUserRepository(IMongoDatabase database)
         {
             userCollection = database.GetCollection<UserEntity>(CollectionName);
+            var indexKeysDefinition = Builders<UserEntity>.IndexKeys.Ascending(u => u.Login);
+            var indexOptions = new CreateIndexOptions { Unique = true };
+            var indexModel = new CreateIndexModel<UserEntity>(indexKeysDefinition, indexOptions);
+            userCollection.Indexes.CreateOne(indexModel);
         }
 
         public UserEntity Insert(UserEntity user)
         {
-            //TODO: Ищи в документации InsertXXX.
-            throw new NotImplementedException();
+            userCollection.InsertOne(user);
+            return user;
         }
 
         public UserEntity FindById(Guid id)
         {
-            //TODO: Ищи в документации FindXXX
-            throw new NotImplementedException();
+            var filter = Builders<UserEntity>.Filter.Eq(u => u.Id, id);
+            return userCollection.Find(filter).FirstOrDefault();
         }
 
         public UserEntity GetOrCreateByLogin(string login)
         {
-            //TODO: Это Find или Insert
-            throw new NotImplementedException();
+            var filter = Builders<UserEntity>.Filter.Eq(u => u.Login, login);
+            var existingUser = userCollection.Find(filter).FirstOrDefault();
+            
+            if (existingUser != null)
+                return existingUser;
+
+            try
+            {
+                var newUser = new UserEntity(Guid.NewGuid(), login, null, null, 0, null);
+                userCollection.InsertOne(newUser);
+                return newUser;
+            }
+            catch (MongoWriteException ex) when (ex.WriteError.Category == ServerErrorCategory.DuplicateKey)
+            {
+                return userCollection.Find(filter).FirstOrDefault();
+            }
         }
 
         public void Update(UserEntity user)
         {
-            //TODO: Ищи в документации ReplaceXXX
-            throw new NotImplementedException();
+            var filter = Builders<UserEntity>.Filter.Eq(u => u.Id, user.Id);
+            userCollection.ReplaceOne(filter, user);
         }
 
         public void Delete(Guid id)
         {
-            throw new NotImplementedException();
+            var filter = Builders<UserEntity>.Filter.Eq(u => u.Id, id);
+            userCollection.DeleteOne(filter);
         }
 
-        // Для вывода списка всех пользователей (упорядоченных по логину)
-        // страницы нумеруются с единицы
         public PageList<UserEntity> GetPage(int pageNumber, int pageSize)
         {
-            //TODO: Тебе понадобятся SortBy, Skip и Limit
-            throw new NotImplementedException();
+            if (pageNumber < 1)
+                throw new ArgumentException("Page number must be greater than 0", nameof(pageNumber));
+                
+            if (pageSize < 1)
+                throw new ArgumentException("Page size must be greater than 0", nameof(pageSize));
+
+            var totalCount = (int)userCollection.CountDocuments(FilterDefinition<UserEntity>.Empty);
+            var users = userCollection
+                .Find(FilterDefinition<UserEntity>.Empty)
+                .SortBy(u => u.Login)
+                .Skip((pageNumber - 1) * pageSize)
+                .Limit(pageSize)
+                .ToList();
+
+            return new PageList<UserEntity>(users, totalCount, pageNumber, pageSize);
         }
 
         // Не нужно реализовывать этот метод
